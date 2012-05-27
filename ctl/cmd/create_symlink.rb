2c967a9d2cd203b1bc1cda149b68369c18bd2787
Antonin Hildebrand <antonin@hildebrand.cz>
2012-05-27 15:16:21 -0700

    symlink creation/deleting needs sudo

--- ctl/cmd/create_symlink.rb
+++ ctl/cmd/create_symlink.rb
@@ -1,5 +1,5 @@
 def cmd_create_symlink(options)
     sys("mkdir -p /usr/local/bin")
-    sys("rm \"#{ASEPSISCTL_SYMLINK_PATH}\"") if File.exists? ASEPSISCTL_SYMLINK_PATH
-    sys("ln -Fs \"#{ASEPSISCTL_SYMLINK_SOURCE_PATH}\" \"#{ASEPSISCTL_SYMLINK_PATH}\"")
+    sys("sudo rm \"#{ASEPSISCTL_SYMLINK_PATH}\"") if File.exists? ASEPSISCTL_SYMLINK_PATH
+    sys("sudo ln -Fs \"#{ASEPSISCTL_SYMLINK_SOURCE_PATH}\" \"#{ASEPSISCTL_SYMLINK_PATH}\"")
 end
--- ctl/cmd/remove_symlink.rb
+++ ctl/cmd/remove_symlink.rb
@@ -1,4 +1,4 @@
 def cmd_remove_symlink(options)
     die "#{ASEPSISCTL_SYMLINK_PATH} does not exist" unless File.exists? ASEPSISCTL_SYMLINK_PATH
-    sys("rm \"#{ASEPSISCTL_SYMLINK_PATH}\"")
+    sys("sudo rm \"#{ASEPSISCTL_SYMLINK_PATH}\"")
 end
