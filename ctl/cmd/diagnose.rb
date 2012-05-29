0b0f65c782e359c2362363ca1cbc641015ef2f2b
Antonin Hildebrand <antonin@hildebrand.cz>
2012-05-28 22:04:01 -0700

    diagnose tests for wrapper library presence

--- ctl/cmd/diagnose.rb
+++ ctl/cmd/diagnose.rb
@@ -49,18 +49,10 @@ def cmd_diagnose(options)
         cry "DesktopServicesPriv has unexpected attributes: #{stat.strip}"
     end
     
-    # if `otool -L "#{ds_lib}" | grep "A_/DesktopServicesPriv"`==""
-    #     cry "DesktopServicesPriv (#{ds_lib}) does not link against /System/Library/PrivateFrameworks/DesktopServicesPriv.framework/Versions/A_/DesktopServicesPriv"
-    # end
-    
-    # test_store_file = File.join(PREFIX_PATH, "tmp", "_DS_Store")
-    # `rm "#{test_store_file}"` if File.exists? test_store_file
-    # `rm /tmp/.DS_Store` if File.exists? "/tmp/.DS_Store"
-    # `touch /tmp/.DS_Store`
-    # unless File.exists? test_store_file then
-    #     puts "tried> touch /tmp/.DS_Store but #{test_store_file} wasn't created - something is wrong"
-    #     is_ok = false
-    # end
+    # this is simple and stupid test: our wrapper library is small, under 100kb
+    if File.size(ds_lib) > 100*1024 then
+        cry "DesktopServicesPriv (#{ds_lib}) is not properly installed.\n  => Have you installed system update recently? It might revert it back to the original version."
+    end
     
     say "Your Asepsis setup seems to be OK" if $is_ok
     exit 1 unless $is_ok
