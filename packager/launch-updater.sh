6ccbeb15c09d4714139cf5964a0f6668b1a9b263
Antonin Hildebrand <antonin@hildebrand.cz>
2014-05-25 20:52:11 +0200

    sleep before doing an update check

--- packager/launch-updater.sh
+++ packager/launch-updater.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# this script is normally launched during startup
+# the system may be busy doing lots of operations, network might be not available yet
+# give system some time before launching the update check
+sleep 180 # 3 minutes
+
 # run the updater check
 /Library/Application\ Support/Asepsis/AsepsisUpdater.app/Contents/MacOS/AsepsisUpdater
