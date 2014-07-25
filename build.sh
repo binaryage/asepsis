92473997a9ecf69ebb348f4363cd331a4b35cab8
Antonin Hildebrand <antonin@hildebrand.cz>
2014-07-25 16:30:10 +0200

    fix paths, min deploy os version = 10.8

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BIN="$PROJECT_DIR/../bin"
+BIN="$PROJECT_DIR/bin"
 mkdir -p "$BIN"
 BIN=`cd "$BIN";pwd` # normalize path
 
@@ -10,21 +10,22 @@ TEST="$TARGET/asepsisTest"
 WRAPPER="$TARGET/DesktopServicesPrivWrapper"
 UPDATER="$TARGET/AsepsisUpdater.app"
 UNINSTALLER="$TARGET/Asepsis Uninstaller.app"
+PACKAGER="$PROJECT_DIR/packager"
 
 echo "build products dir is $BUILT_PRODUCTS_DIR"
 echo "assembling final products in $BIN"
 
 mkdir -p "$TARGET"
 
-cp "$PROJECT_DIR"/com.binaryage.*.plist "$TARGET"
-cp "$PROJECT_DIR/finish-uninstall.sh" "$TARGET"
-cp "$PROJECT_DIR/finish-kext-uninstall.sh" "$TARGET"
-cp "$PROJECT_DIR/launch-updater.sh" "$TARGET"
+cp "$PACKAGER"/com.binaryage.*.plist "$TARGET"
+cp "$PACKAGER/finish-uninstall.sh" "$TARGET"
+cp "$PACKAGER/finish-kext-uninstall.sh" "$TARGET"
+cp "$PACKAGER/launch-updater.sh" "$TARGET"
 cp -Rf "$BUILT_PRODUCTS_DIR/asepsisd" "$DAEMON"
 cp -Rf "$BUILT_PRODUCTS_DIR/asepsisTest" "$TEST"
 cp -Rf "$BUILT_PRODUCTS_DIR/DesktopServicesPrivWrapper.framework/Versions/A/DesktopServicesPrivWrapper" "$WRAPPER"
 cp -Rf "$BUILT_PRODUCTS_DIR/AsepsisUpdater.app" "$UPDATER"
-cp "$PROJECT_DIR/install_name_tool" "$TARGET"
+cp "$PACKAGER/install_name_tool" "$TARGET"
 
 cp -Rf "$BUILT_PRODUCTS_DIR/TotalFinder Uninstaller.app/" "$UNINSTALLER"
 mv "$UNINSTALLER/Contents/MacOS/TotalFinder Uninstaller" "$UNINSTALLER/Contents/MacOS/Asepsis Uninstaller"
@@ -34,4 +35,4 @@ install_name_tool -change "/System/Library/PrivateFrameworks/DesktopServicesPriv
 rm "$UPDATER/Contents/Frameworks/Sparkle.framework/Headers"
 rm -rf "$UPDATER/Contents/Frameworks/Sparkle.framework/Versions/A/Headers"
 
-cp -Rf "$PROJECT_DIR/../ctl" "$TARGET"
+cp -Rf "$PROJECT_DIR/ctl" "$TARGET"
